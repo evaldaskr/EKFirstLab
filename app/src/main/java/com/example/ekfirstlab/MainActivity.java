fd3ace9481715a83504709663a269cc1887a2da5
evaldaskr <evaldas.krampas@gmail.com>
2022-03-24 23:38:33 +0200

    Text now changes after click of a button.

--- app/src/main/java/com/example/ekfirstlab/MainActivity.java
+++ app/src/main/java/com/example/ekfirstlab/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ekfirstlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void OnBtnClick(View view) {
+        this.tvMain.setText(R.string.changed_label_caption);
     }
 }
